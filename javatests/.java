03b83d8d77beee6b31fc9f5d4af208e1e6f98f19
rayanesabi <rayane.sabi@etu.univ-amu.fr>
2023-04-02 17:09:24 +0200

    COMPILATEUR NON FONCTIONNEL L'OUTPUT NE RENVOIE RIEN

---
+++ javatests/.java
@@ -0,0 +1,14 @@
+package javatests;
+public class Pirate {
+
+    public static void main(String[] args) {
+        /* Afficher le nom du pirate */
+
+        /* Afficher la taille du pirate */
+
+        /* Afficher la pointure du pirate */
+
+        /* Afficher la lettre du pirate */
+
+    }
+}
